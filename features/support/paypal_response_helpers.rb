9e4869ef6315ddc84d734c42d6f63d70e6fddca6
David Wilkie <dwilkie@gmail.com>
2010-11-19 14:34:38 +0700

    Moved code into integration_test_helper so it can be reused across other apps

--- features/support/paypal_response_helpers.rb
+++ features/support/paypal_response_helpers.rb
@@ -1,14 +1,2 @@
-module PaypalResponseHelpers
-  def register_get_user_details_response
-    body_template = "PAYERID=RK7XZT4AUY79C&TIMESTAMP=2010%2d10%2d07T10%3a08%3a44Z&CORRELATIONID=83dfe25684ced&ACK=Success&VERSION=2%2e3&BUILD=1545724"
-    paypal_response = Rack::Utils.parse_nested_query(body_template)
-    paypal_response.merge!(@paypal_user_details)
-    response_body = Rack::Utils.build_nested_query(paypal_response)
-    FakeWeb.register_uri(
-      :post, Paypal.nvp_uri, :body => response_body
-    )
-  end
-end
-
-World(PaypalResponseHelpers)
+World(DevisePaypal::IntegrationTestHelper)
 
---
+++ lib/devise_paypal/integration_test_helper.rb
@@ -0,0 +1,14 @@
+module DevisePaypal
+  module IntegrationTestHelper
+    def register_get_user_details_response(paypal_user_details)
+      body_template = "PAYERID=RK7XZT4AUY79C&TIMESTAMP=2010%2d10%2d07T10%3a08%3a44Z&CORRELATIONID=83dfe25684ced&ACK=Success&VERSION=2%2e3&BUILD=1545724"
+      paypal_response = Rack::Utils.parse_nested_query(body_template)
+      paypal_response.merge!(paypal_user_details)
+      response_body = Rack::Utils.build_nested_query(paypal_response)
+      FakeWeb.register_uri(
+        :post, Paypal.nvp_uri, :body => response_body
+      )
+    end
+  end
+end
+
